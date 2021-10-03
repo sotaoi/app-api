605a2eae8323874c837f968dc800bb9a9aaf1aac
qwertypnk <qwertypnk@protonmail.com>
2021-10-03 15:49:31 +0300

    refactor: update

--- scripts/db-migrate.ts
+++ scripts/db-migrate.ts
@@ -1,13 +1,20 @@
 import { main as apiMain } from '@app/api/main';
 import { migrate } from '@sotaoi/api/db';
 import { logger } from '@sotaoi/api/logger';
+import { cpanelMigrate } from '@sotaoi/api/db/cpanel-tables';
+import { Store } from '@sotaoi/api/store';
 
 const main = async (): Promise<void> => {
   try {
+    logger().info(`Running app migrations...\n`);
     await apiMain(true);
     const migrations = (await migrate())[1];
     !migrations.length && (migrations[0] = 'No new migrations');
     logger().info(`\n\nMigrate command complete:\n${migrations.join('\n')}\n`);
+
+    logger().info(`Running control panel migrations...\n`);
+    await cpanelMigrate(Store.getAppInfo().isMasterBundle === 'yes');
+
     process.exit(0);
   } catch (err) {
     logger().estack(err);
