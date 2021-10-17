9282263a1d4be99d3cae9d1b902a8bb68b6c1da4
qwertypnk <qwertypnk@protonmail.com>
2021-10-17 11:16:53 +0300

    refactor: update

--- scripts/deploy/deploy-aapi.ts
+++ scripts/deploy/deploy-aapi.ts
@@ -27,7 +27,33 @@ const main = async () => {
     path.resolve('./tmp.deployment'),
   ]);
 
+  execSync('npx tsc', { cwd: path.resolve('./deployment'), stdio: 'inherit' });
+  fs.unlinkSync(path.resolve('./deployment/tsconfig.json'));
   execSync('npm run bootstrap:prod', { cwd: path.resolve('./deployment'), stdio: 'inherit' });
+
+  Helper.iterateRecursiveSync(
+    fs,
+    path,
+    path.resolve('./deployment'),
+    (item) => {
+      if (fs.lstatSync(item).isDirectory()) {
+        return;
+      }
+      item = path.resolve(item);
+      if (item.substr(-3) === '.ts' && item.substr(-5) !== '.d.ts') {
+        const filename = item.substr(0, item.length - 3);
+        fs.existsSync(`${filename}.ts`) &&
+          fs.lstatSync(`${filename}.ts`).isFile() &&
+          fs.existsSync(`${filename}.js`) &&
+          fs.lstatSync(`${filename}.js`).isFile() &&
+          fs.existsSync(`${filename}.d.ts`) &&
+          fs.lstatSync(`${filename}.d.ts`).isFile() &&
+          fs.unlinkSync(item);
+      }
+      item.substr(-4) === '.tsx' && fs.unlinkSync(item);
+    },
+    [path.resolve('./deployment/node_modules')],
+  );
 
   fs.renameSync(path.resolve('./tmp.deployment/.git'), path.resolve('./deployment/.git'));
   fs.rmdirSync(path.resolve('./tmp.deployment'), { recursive: true });
