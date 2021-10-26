1bf7e95560e26f610781edc0bce41b6a9ae25c70
qwertypnk <qwertypnk@protonmail.com>
2021-10-26 09:19:36 +0300

    0.9.1

--- scripts/db/db-seed.ts
+++ scripts/db/db-seed.ts
@@ -3,15 +3,21 @@
 import { main } from '@app/api/main';
 import { seed } from '@sotaoi/api/db';
 import { logger } from '@sotaoi/api/logger';
+import { getAppInfo } from '@sotaoi/omni/get-app-info';
+import { cpanelSeed } from '@sotaoi/api/db/cpanel-tables';
 
 const run = async (): Promise<void> => {
   try {
     await main(true);
+    logger().info(`Running app seeds...\n`);
 
     const seeds = (await seed())[0];
     !seeds.length && (seeds[0] = 'No seed files');
-
     logger().info(`\n\nSeeding complete:\n${seeds.join('\n')}\n`);
+
+    logger().info(`Running control panel seeds...\n`);
+    await cpanelSeed(getAppInfo().isMasterBundle === 'yes');
+
     process.exit(0);
   } catch (err) {
     logger().estack(err);
