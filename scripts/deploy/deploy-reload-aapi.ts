e1c308e72e1ca7f1296a7e8672bac8849a7d232d
qwertypnk <qwertypnk@protonmail.com>
2021-10-27 09:52:16 +0300

    fix: smallfix

--- scripts/deploy/deploy-reload-aapi.ts
+++ scripts/deploy/deploy-reload-aapi.ts
@@ -3,7 +3,7 @@
 import { execSync } from 'child_process';
 
 const main = async () => {
-  execSync('git branch -D tmp/deploy', { stdio: 'inherit' });
+  execSync('git branch -D tmp/deploy > /dev/null 2>&1', { stdio: 'inherit' });
   execSync('git checkout -b tmp/deploy', { stdio: 'inherit' });
   execSync('git fetch && git pull', { stdio: 'inherit' });
   execSync('git checkout -- ./', { stdio: 'inherit' });
